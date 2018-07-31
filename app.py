e9bf3428561d57eafd1fc312326bf4dd4d2b6fec
Rahandi Noor Pasha <rahandi16@mhs.if.its.ac.id>
2018-07-31 17:57:48 +0700

    +error checking setelah process image

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ from tqdm import trange
 
 def create3D(greyed, image_name):
     fid = open('result_{}.obj'.format(image_name), 'w')
-    scl = 20
-    med = np.median(greyed) #np.max(greyed) kalau mau timbul
+    scl = 20 #ngatur ketinggian
+    med = np.max(greyed) #np.max(greyed) kalau mau timbul, np.median(greyed) kalau cekung
     rows, columns = greyed.shape
-    counter = 0
     for a in trange(rows, desc='create vertex'):
         for b in range(columns):
-            if greyed[a][b] < 0.99:
+            if greyed[a][b] == 0:
                 t = greyed[a][b]
                 f = t*med/255.0*scl
             elif greyed[a][b] > med:
@@ -21,7 +20,6 @@ def create3D(greyed, image_name):
                 t = greyed[a][b]
                 f = t/255.0*scl
             fid.write('v {} {} {}\n'.format(a+1, b+1, f))
-            counter = counter + 1
     fid.write('s 1\n')
     x = rows * columns
     for d in trange(1, x+1, desc='create triangle mesh'):
@@ -40,27 +38,29 @@ def create3D(greyed, image_name):
 def process_image(image_object, image_name):
     copied = image_object.copy()
     edged = cv2.Canny(copied, 10, 250)
-    cv2.imshow('edged', edged)
+    # cv2.imshow('edged', edged)
     kernel = np.ones((5, 5), np.uint8)
     dilation = cv2.dilate(edged, kernel, iterations=1)
-    cv2.imshow('dilation', dilation)
+    # cv2.imshow('dilation', dilation)
     closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
-    cv2.imshow('closing', closing)
+    # cv2.imshow('closing', closing)
     (image, cnts, hiers) = cv2.findContours(closing, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cont = cv2.drawContours(copied, cnts, -1, (0, 0, 0), 1, cv2.LINE_AA)
-    cv2.imshow('contour', cont)
+    # cv2.imshow('contour', cont)
     mask = np.zeros(cont.shape[:2], dtype="uint8") * 255
     cv2.drawContours(mask, cnts, -1, (255, 255, 255), -1)
     img = cv2.bitwise_and(cont, cont, mask=mask)
-    cv2.imshow('masked', img)
+    # cv2.imshow('masked', img)
+    # cv2.waitKey(0)
+    new_img = None
     for c in cnts:
         x, y, w, h = cv2.boundingRect(c)
         if w > 50 and h > 130:
             new_img = img[y:y + h, x:x + w]
-            cv2.imwrite('cropped_{}.png'.format(image_name), new_img)
-            cv2.waitKey(0)
-            print('cropped_{}.png created'.format(image_name))
-            return new_img
+    if new_img is not None:
+        cv2.imwrite('cropped_{}.png'.format(image_name), new_img)
+        print('cropped_{}.png created'.format(image_name))
+    return new_img
 
 if __name__ == '__main__':
     image_path = sys.argv[1:]
@@ -86,5 +86,9 @@ if __name__ == '__main__':
                 m = 1
         resized_image = imresize(image_object, m)
         new_image = process_image(resized_image, image_name.split('.')[0])
-        create3D(new_image, image_name.split('.')[0])
+        if new_image is not None:
+            create3D(new_image, image_name.split('.')[0])
+        else:
+            print('image processing failed, create 3d from original image')
+            create3D(resized_image, image_name.split('.')[0])
         print('\n')
